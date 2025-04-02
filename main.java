fa89f705492c72629a2c5c836e7562103d8254b1
ceYASH <yashthakurce@gmail.com>
2025-04-02 23:41:50 +0530

    main file

---
+++ main.java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main(String[] args){
+        byte age = 34;
+        int age2 = 56;
+        char ch ='A';
+        float f1 = 5.6f;
+        double d1 = 6.7d;
+        boolean a = true;
+        System.out.println(age);
+    }
+}
